8e517b7dc539da92092e65de02ba0c5e4a09892b
Sveste123 <67866253+Sveste123@users.noreply.github.com>
2023-03-22 17:05:13 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let saldo = 1526;
+
+  let history = [
+    "pantere i høst = xx",
+    "1mda-kafé = kr +1596",
+    "roser og sjokolade til kontakt-lærerne våre, (valentinsdagen) = kr -170",
+    "Erik panta = kr +100",
+  ];
+</script>
+
+<div class="saldo">
+  <h1>Saldo: kr {saldo}</h1>
+</div>
+
+<div class="historikk">
+  <h2>Historikk:</h2>
+  <ul>
+    {#each history as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+</div>
